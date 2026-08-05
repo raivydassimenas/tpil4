6719a6aca050c4f32784d9489bce05fa3698152c
Raivydas Šimėnas <raivydas.simenas@proton.me>
2026-08-05 16:26:43 +0300

    Prove commutativity of or

--- Tpil4/props_and_proofs.lean
+++ Tpil4/props_and_proofs.lean
@@ -9,7 +9,20 @@ example : p ∧ q ↔ q ∧ p :=
       show p ∧ q from And.intro (And.right h) (And.left h))
 
 
-example : p ∨ q ↔ q ∨ p := sorry
+example : p ∨ q ↔ q ∨ p :=
+  Iff.intro
+    (fun h : p ∨ q =>
+      Or.elim h
+      (fun hp : p =>
+        show q ∨ p from Or.intro_right q hp)
+      (fun hq : q =>
+        show q ∨ p from Or.intro_left p hq))
+    (fun h : q ∨ p =>
+      Or.elim h
+      (fun hq : q =>
+        show p ∨ q from Or.intro_right p hq)
+      (fun hp : p =>
+        show p ∨ q from Or.intro_left q hp))
 
 -- associativity of ∧ and ∨
 example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
