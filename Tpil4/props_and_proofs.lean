14bfb6f01096c314a8508da5320e383fad7d68c2
Raivydas Šimėnas <raivydas.simenas@proton.me>
2026-09-02 16:07:59 +0300

    Prove False equivalence and modus tolens

--- Tpil4/props_and_proofs.lean
+++ Tpil4/props_and_proofs.lean
@@ -200,5 +200,16 @@ example : p ∨ False ↔ p :=
           h.elim))
     (fun hp : p =>
       Or.inl hp)
-example : p ∧ False ↔ False := sorry
-example : (p → q) → (¬q → ¬p) := sorry
+
+example : p ∧ False ↔ False :=
+  Iff.intro
+    (fun hpf : p ∧ False =>
+      hpf.right.elim)
+    (fun hf : False =>
+      hf.elim)
+
+example : (p → q) → (¬q → ¬p) :=
+  fun hpq : p → q =>
+    fun hnq : ¬q =>
+      fun hp : p =>
+        absurd (hpq hp) hnq
